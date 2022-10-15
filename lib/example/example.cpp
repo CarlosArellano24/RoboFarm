0a046b9002ad498ea5479233b813e659978be769
CarlosArellano24 <78268650+CarlosArellano24@users.noreply.github.com>
2022-10-15 18:50:47 -0500

    🗑 Example Files Removed

--- lib/example/example.cpp
+++
@@ -1,9 +0,0 @@
-#include "example.h"
-
-bool example_function(void) {
-  return true;
-}
-
-int example_value(void) {
-  return 5;
-}
--- lib/example/example.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include <Arduino.h>
-
-bool example_function(void);
-int example_value(void);
